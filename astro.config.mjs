efdb17bda6806f7d370ae710f0d869c9277c0575
profWilliamArte <arteagawilliam13@hotmail.com>
2024-03-08 17:33:59 -0400

    Primer Commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://profwilliamarte.github.io',
+    base: '/jsonserver',
+    build: {
+      assets: 'astro'
+    }
+
+});
